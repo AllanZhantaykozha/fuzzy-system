051aead4007f63a5ac8be104d7dbd5652040152a
theallan765 <103952115+AllanZhantaykozha@users.noreply.github.com>
2024-10-08 18:29:57 +0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Page from "./components/Page";
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Page />
+    </div>
+  );
+}
+
+export default App;
